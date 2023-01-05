f761f4b27ad4f85ecdb17ff20cdd563cf488c21d
takuya shimizu <p.t.901.ts@gmail.com>
2023-01-05 23:06:45 +0900

    課題要件の全てを実装

--- config/application.rb
+++ config/application.rb
@@ -7,5 +7,12 @@ Bundler.require(*Rails.groups)
 module SessionLogin
   class Application < Rails::Application
     config.load_defaults 6.1
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,5 @@ Rails.application.routes.draw do
     end
   end
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-  resources :blogs do
-    collection do
-      post :confirm
-    end
-  end
   resources :favorites, only: [:create, :destroy, :index]
 end
